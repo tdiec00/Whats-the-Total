d55f2eeb846c2909aa310aa002dc85d84af6b2b3
Trung Diec <tdiec00@gmail.com>
2022-02-25 16:33:20 -0500

    sign up form completed

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,5 +1,44 @@
-import React from "react"
+import {useState} from "react"
+import {useNavigate} from "react-router-dom"
+import {loginUser} from "../../services/users"
 
-export default function Login() {
-  return <div>Login</div>
+export default function Login(props) {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const newUser = {
+      username,
+      password,
+    }
+    const resp = await loginUser(newUser)
+    props.setCurrentUser(resp)
+    navigate("/products")
+  }
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value)
+        }}
+        placeholder="username"
+      />
+      <br />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value.toString())
+        }}
+        placeholder="password must be 6 digits long"
+      />
+      <br />
+      <button>Login</button>
+    </form>
+  )
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,13 +3,16 @@ import {Link} from "react-router-dom"
 export default function Navbar(props) {
   return (
     <div>
-      <Link to="/">Home</Link>
       {props.currentUser ? (
         <>
+          <Link to="/">Home</Link>
+          <Link to="/shopping-cart">Shopping Cart</Link>
+          <button onClick={props.logOut}>Logout</button>
           <h3>Welcome, {props.currentUser.username}. Happy shopping!!</h3>
         </>
       ) : (
         <>
+          <Link to="/">Home</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
         </>
