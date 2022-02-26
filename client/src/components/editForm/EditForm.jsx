8e75be95dc263dc10680f0a849f4cea734ef68e1
Trung Diec <tdiec00@gmail.com>
2022-02-26 14:39:22 -0500

    edit product form

--- client/src/components/editForm/EditForm.jsx
+++ client/src/components/editForm/EditForm.jsx
@@ -3,7 +3,18 @@ import React from "react"
 export default function EditForm() {
   return (
     <form>
-      <input></input>
+      <input placeholder="name" />
+      <input placeholder="price" />
+      <select>
+        <option value="fruits" selected>
+          Fruits
+        </option>
+        <option value="vegetables">Vegetables</option>
+        <option value="frozen">Frozen Foods</option>
+        <option value="drinks">Drinks</option>
+        <option value="meats">Meats</option>
+        <option value="dairy">Dairy</option>
+      </select>
     </form>
   )
 }
