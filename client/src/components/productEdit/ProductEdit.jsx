8f066a590743439aae0672553bfdc4d909cfc0cf
Trung Diec <tdiec00@gmail.com>
2022-02-26 14:00:13 -0500

    edit button works

--- client/src/components/productEdit/ProductEdit.jsx
+++ client/src/components/productEdit/ProductEdit.jsx
@@ -3,5 +3,5 @@ import {useNavigate} from "react-router-dom"
 export default function ProductEdit() {
   const navigate = useNavigate()
 
-  return <button onClick={navigate("/products/edit")}>ProductEdit</button>
+  return <button onClick={() => navigate("/products/edit")}>ProductEdit</button>
 }
