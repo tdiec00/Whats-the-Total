7b139fff680ed4d682b48c9c7cae2c3467c40835
Trung Diec <tdiec00@gmail.com>
2022-03-02 10:46:33 -0500

    loading screen created

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,16 @@ import ProductsContainer from "./components/productsContainer/ProductsContainer"
 import Cart from "./screens/cart/Cart"
 import EditProductModal from "./components/editProductModal/EditProductModal"
 import ProductModal from "./components/productModal/ProductModal"
+import {FillingBottle} from "react-cssfx-loading"
 import "./App.css"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000)
+  })
+
   const [currentUser, setCurrentUser] = useState(null)
   const navigate = useNavigate()
 
@@ -31,20 +38,29 @@ function App() {
   }
 
   return (
-    <div className="main-body">
-      <Navbar logOut={logOut} currentUser={currentUser} />
-      <div className="page-container">
-        <Routes>
-          <Route path="/" element={<Home setCurrentUser={setCurrentUser} />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/shopping-cart" element={<Cart />} />
-          <Route path="/products/:category" element={<ProductsContainer />} />
-          <Route path="/products/edit/:id" element={<EditProductModal />} />
-          <Route path="/products/add" element={<ProductModal />} />
-        </Routes>
-      </div>
-      <Footer logOut={logOut} currentUser={currentUser} />
-    </div>
+    <>
+      {loading === false ? (
+        <div className="main-body">
+          <Navbar logOut={logOut} currentUser={currentUser} />
+          <div className="page-container">
+            <Routes>
+              <Route path="/" element={<Home setCurrentUser={setCurrentUser} />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/shopping-cart" element={<Cart />} />
+              <Route path="/products/:category" element={<ProductsContainer />} />
+              <Route path="/products/edit/:id" element={<EditProductModal />} />
+              <Route path="/products/add" element={<ProductModal />} />
+            </Routes>
+          </div>
+          <Footer logOut={logOut} currentUser={currentUser} />
+        </div>
+      ) : (
+        <div className="loading">
+          <h1>Page is loading...</h1>
+          <FillingBottle className="logo" />
+        </div>
+      )}
+    </>
   )
 }
 
