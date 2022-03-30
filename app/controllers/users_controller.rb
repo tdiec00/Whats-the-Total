62798d73dab4da58ecab819c6dcad28c29b48dc3
Trung Diec <tdiec00@gmail.com>
2022-03-30 11:07:13 -0400

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,8 +43,7 @@ def update_count
   @user = User.find(params[:id])
   @product = @user.products.find(params[:product_id])
   @product.number = 5
-  # @user.products.delete(params[:product_id])
-  @user.products << @product
+  @user.save
   render json: @user
   # render json: @user.products
 end
