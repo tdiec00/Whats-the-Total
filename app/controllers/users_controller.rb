3e31c4b51d6f55a103817371b80c93901339ff8c
Trung Diec <tdiec00@gmail.com>
2022-03-30 00:52:18 -0400

    controller testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
 def update_count
   @user = User.find(params[:id])
   @product = @user.products.find(params[:product_id])
-  render json: params[:number]
+  render json: {number:params[:number]}
   # if @product.update({number: params[:number]})
   #   render json: @product
   # else
