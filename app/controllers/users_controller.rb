5f9d5a4844a45db123d391178274fcd473de4cbe
Trung Diec <tdiec00@gmail.com>
2022-04-02 12:06:01 -0400

    controller testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,16 +54,16 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    p @user
-    if @user.save
-      @token = encode({id: @user.id})
-      render json: {
-        user: @user.attributes.except("password_digest"),
-        token: @token
-        }, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    render json: @user
+    # if @user.save
+    #   @token = encode({id: @user.id})
+    #   render json: {
+    #     user: @user.attributes.except("password_digest"),
+    #     token: @token
+    #     }, status: :created
+    # else
+    #   render json: @user.errors, status: :unprocessable_entity
+    # end
   end
 
   # PATCH/PUT /users/1
