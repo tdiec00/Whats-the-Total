4c99303de8be64d67c601be518e96d316f5382f2
Trung Diec <tdiec00@gmail.com>
2022-04-02 03:15:37 -0400

    styled tax container

--- client/src/components/shoppingTotal/ShoppingTotal.jsx
+++ client/src/components/shoppingTotal/ShoppingTotal.jsx
@@ -1,11 +1,21 @@
+import states from "../../utilities/saleTax.json"
+
 export default function ShoppingTotal(props) {
   let total = 0
   props.products.forEach((product) => {
     total += product.price
   })
+  let rate = Object.entries(states.states).filter((state) => {
+    return state[0] === "MA"
+  })
+  let taxRate = rate[0][1]?.rate
+  let tax = Number((total.toFixed(2) * taxRate).toFixed(2))
+  let finalTotal = total + tax
+
   return (
     <div className="total-container">
-      <p>Your Total: ${total.toFixed(2)}</p>
+      <p className="total-text-container">Tax: ${tax}</p>
+      <p>Your Total: ${finalTotal.toFixed(2)}</p>
     </div>
   )
 }
