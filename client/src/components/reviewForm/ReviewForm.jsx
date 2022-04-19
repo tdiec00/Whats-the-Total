1c0589d81620a4c52e966581d809c30dcd872296
Trung Diec <tdiec00@gmail.com>
2022-04-19 11:47:42 -0400

    updates on review form

--- client/src/components/reviewForm/ReviewForm.jsx
+++ client/src/components/reviewForm/ReviewForm.jsx
@@ -1,20 +1,31 @@
+import {useState} from "react"
+import {Rating} from "react-simple-star-rating"
 import "./reviewForm.css"
 
 export default function ReviewForm() {
+  const [rating, setRating] = useState(0)
+  const [readonly, setReadonly] = useState(false)
+  const handleRating = (rate) => {
+    setRating(rate / 20)
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    setRating(0)
+  }
+
   return (
-    <form className="review-form">
-      <label>Add a new review</label>
-      <br />
-      <textarea rows="20" placeholder="Please leave your review here"></textarea>
-      <br />
-      <select>
-        <option value="none">Rate this product</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-    </form>
+    <div>
+      <form className="review-form">
+        <label>Review this product</label>
+        <br />
+        <textarea rows="20" placeholder="Please leave your review here"></textarea>
+        <br />
+      </form>
+      <div>
+        <Rating onClick={handleRating} ratingValue={rating} fillColorArray={["#f50b0b", "#f4562a", "#f27a2a", "#f1b345", "#f1d045"]} readonly={readonly} allowHalfIcon />
+        <p>{rating} stars</p>
+      </div>
+    </div>
   )
 }
