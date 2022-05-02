6be5f7ec9bc8faf36d4708998dee490efe830e19
Trung Diec <tdiec00@gmail.com>
2022-05-02 11:59:10 -0400

    added notes to state dropdown

--- client/src/components/stateDropdown/StateDropdown.jsx
+++ client/src/components/stateDropdown/StateDropdown.jsx
@@ -4,6 +4,7 @@ export default function StateDropdown(props) {
   return (
     <select onChange={(event) => props.setState(event.target.value)}>
       <option>Select a State</option>
+      //maps through the key value pairs in state object to display dropdown of all states
       {Object.keys(states.states).map((state, value) => {
         return (
           <option key={value} value={state}>
