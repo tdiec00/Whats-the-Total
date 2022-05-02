e12cc9b56c53384c3a35d52cbf3f5525ba5633ed
Trung Diec <tdiec00@gmail.com>
2022-05-02 12:06:55 -0400

    added comments to shopping total component

--- client/src/components/shoppingTotal/ShoppingTotal.jsx
+++ client/src/components/shoppingTotal/ShoppingTotal.jsx
@@ -4,20 +4,27 @@ export default function ShoppingTotal(props) {
   let total = 0
   let stateTax = localStorage.getItem("state")
 
+  //add the the prices of the items to the total
   props.products.forEach((product) => {
     total += product.price
   })
+
+  //access the state tax rates object to filter and find the tax rate for the customers state
   let rate = Object.entries(states.states).filter((state) => {
     return state[0] === stateTax
   })
   let taxRate = rate[0][1]?.rate
-  let tax = Number((total.toFixed(2) * taxRate).toFixed(2))
+
+  //calculate the total cost of tax
+  let taxTotal = Number((total.toFixed(2) * taxRate).toFixed(2))
+
+  //the final total of the total including tax
   let finalTotal = total + tax
 
   return (
     <div className="total-container">
       <p className="subtotal-container">Subtotal: ${total.toFixed(2)}</p>
-      <p className="total-text-container">Tax: ${tax.toFixed(2)}</p>
+      <p className="total-text-container">Tax: ${taxTotal.toFixed(2)}</p>
       <p>Your Total: ${finalTotal.toFixed(2)}</p>
     </div>
   )
