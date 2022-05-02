c4cea2294d4182c31b1f363dd221f513e22ea499
Trung Diec <tdiec00@gmail.com>
2022-05-02 12:08:57 -0400

    comments for product total  component

--- client/src/components/productTotal/ProductTotal.jsx
+++ client/src/components/productTotal/ProductTotal.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 
 export default function ProductTotal(props) {
+  //display the total of a specific product including the quantity of each product
+  //example 5 apples * $1.00 = $5.00
   let count = props.products?.filter((product) => product.id === props.list_id).length
   let total = count * props.price
   return <h4>${total.toFixed(2)}</h4>
